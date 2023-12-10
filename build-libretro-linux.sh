44e485aa48b5397b177d8422a2aff3d0c8bbdeb4
zorn <zorn7@yandex.ru>
2023-12-10 19:51:05 +1000

    Add build script for windows

--- build-libretro-linux.sh
+++ build-libretro-linux.sh
@@ -3,7 +3,7 @@
 set -e
 
 cd $(dirname $0)
-mkdir -p build && cd build
+mkdir -p build-linux && cd build-linux
 cmake -DENABLE_TESTS=OFF -DENABLE_DEDICATED_ROOM=OFF \
     -DENABLE_SDL2=OFF -DENABLE_QT=OFF -DENABLE_WEB_SERVICE=OFF -DENABLE_SCRIPTING=OFF \
     -DENABLE_OPENAL=OFF -DENABLE_LIBUSB=OFF -DCITRA_ENABLE_BUNDLE_TARGET=OFF \
