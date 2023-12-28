6cf80643e6bd76c9ab3d667b013fc19d307bfd30
zorn <zorn7@yandex.ru>
2023-12-29 07:08:20 +1000

    Fix mingw build

--- build-libretro-windows.sh
+++ build-libretro-windows.sh
@@ -5,7 +5,7 @@ set -e
 cd $(dirname $0)
 mkdir -p build-windows && cd build-windows
 cmake -DCMAKE_TOOLCHAIN_FILE=../CMakeModules/x86_64-w64-mingw32.cmake \
-    -DCITRA_WARNINGS_AS_ERRORS=OFF -DDISABLE_CLANG_TARGET=ON \
+    -DCITRA_WARNINGS_AS_ERRORS=OFF -DDISABLE_CLANG_TARGET=ON -DENABLE_LTO=OFF \
     -DENABLE_TESTS=OFF -DENABLE_DEDICATED_ROOM=OFF \
     -DENABLE_SDL2=OFF -DENABLE_QT=OFF -DENABLE_WEB_SERVICE=OFF -DENABLE_SCRIPTING=OFF \
     -DENABLE_OPENAL=OFF -DENABLE_LIBUSB=OFF -DCITRA_ENABLE_BUNDLE_TARGET=OFF \
@@ -13,6 +13,6 @@ cmake -DCMAKE_TOOLCHAIN_FILE=../CMakeModules/x86_64-w64-mingw32.cmake \
     ..
 make -j$(nproc)
 
-#SO=citra_libretro.dll
-#echo "Core file is here => $(readlink -f $SO)"
-#x86_64-w64-mingw32-strip $SO
+SO=bin/Release/citra_libretro.dll
+echo "Core file is here => $(readlink -f $SO)"
+x86_64-w64-mingw32-strip $SO
